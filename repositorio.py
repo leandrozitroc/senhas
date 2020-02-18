fd05636a551e06f474ba1728b99e80dfcdd35e36
Leandro Cortiz <49104364+leandrozitroc@users.noreply.github.com>
2020-02-18 11:11:43 -0300

    refinamentos no repositorio.py

--- repositorio.py
+++ repositorio.py
@@ -1,9 +1,25 @@
 #! Python
 import sys, pyperclip
-todas = {'hotmail': "10", 'gmail1': '11', 'gmail2': '12' }
+from senhas.luli import decript_senha
 
-conta = input('Insira a conta: ')
+def repositorio():
+    print('==='* 18)
+    print('==='* 18)
+    print('\33[1;34mBem vindo ao Repositorio de senhas\33[m'.center(20 *3))
+    print('==='* 18)
+    print('==='* 18)
+    todas = {'hotmail': "gAAAAABeS-fw8uY9Ebbr8vHOSEP5LJZr4AU4otO-aZ_S_qn0TfKGLNO5AoFe41KE1L2oLYkFtzKPTUTHtixKIIBlC6oYIz-NGQ==",
+             'gmail1': 'gAAAAABeS-fw8uY9Ebbr8vHOSEP5LJZr4AU4otO-aZ_S_qn0TfKGLNO5AoFe41KE1L2oLYkFtzKPTUTHtixKIIBlC6oYIz-NGQ==',
+             'gmail2': 'gAAAAABeS-fw8uY9Ebbr8vHOSEP5LJZr4AU4otO-aZ_S_qn0TfKGLNO5AoFe41KE1L2oLYkFtzKPTUTHtixKIIBlC6oYIz-NGQ==' }
 
-if conta in todas:
-    pyperclip.copy(todas[conta])
-    print('Senha copiada')
+    print(f'As contas disponiveis neste repositório de senha são: ')
+    print('==='* 18)
+
+    for i,  key  in enumerate(todas.keys()):
+        print(f'{i:<3} {key:<9} ')
+    print('===' * 18)
+    conta = input('Insira o nome da conta: ')
+
+    if conta in todas:
+        decript_senha(todas[conta], 'ccLmrWCJu7vpwaroDuSEjv6amtpLyiY9MI-PS-oWy_4=')
+
--- sistema_athena.py
+++ sistema_athena.py
@@ -1,5 +1,6 @@
 from senhas.valida import valida
 from senhas.luli import *
+from senhas.repositorio import *
 print('==='* 18)
 print('==='* 18)
 print('\33[1;34mBem vindo ao sistema Athena\33[m'.center(20 *3))
@@ -13,7 +14,8 @@ while True:
             '[3] Descriptografar Senha: \n'              
             '[4] Criptografar Arquivo: \n'   
             '[5] Descriptografar Arquvivo: \n'
-            '[6] Sair')
+            '[6] Acessar Repositorio de Senhas:\n'
+            '[7] Sair')
       print('==='* 18)
       escolha = valida('Digite uma opção: ')
       if escolha == 1:
@@ -28,7 +30,9 @@ while True:
             decript_senha(input('Senha a ser decriptada: '), "ccLmrWCJu7vpwaroDuSEjv6amtpLyiY9MI-PS-oWy_4=")
       elif escolha == 5:
             decript_arquivo(input('Digite o Arquivo que sera Decriptado: '), "ccLmrWCJu7vpwaroDuSEjv6amtpLyiY9MI-PS-oWy_4=")
-      elif escolha ==6:
+      elif escolha == 6:
+            repositorio()
+      elif escolha ==7:
             print('==='* 18)
             print('\33[4;36mObrigado por usar o sistema Athena\33[m ')
             break
